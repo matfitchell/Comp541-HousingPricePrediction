11b822e0bd5c0cec378b5634eccab4416300cf8e
matfitchell <64942086+matfitchell@users.noreply.github.com>
2022-10-09 16:58:34 -0700

    Create crawler.r

---
+++ Crawler/crawler.r
@@ -0,0 +1,33 @@
+# Inspired by https://github.com/notesofdabbler
+
+library(rvest)
+
+url <- "https://www.zillow.com/homes/"
+url <- paste0(url, "portland")
+
+page <- read_html(url)
+
+houses <- page %>%
+  html_elements(".photo-cards li article")
+
+##z_id <- houses %>% html_attr("id")
+
+##address <- houses %>%
+##  html_element(".zsg-photo-card-address") %>%
+##  html_text()
+prices <- c()
+i <- 0
+for(house in houses) {
+  price <- houses[i] %>%
+    html_element(xpath="//div/div[1]/div[2]/span") %>%
+    html_text() %>%
+    readr::parse_number()
+  append(prices, price)
+  i <- i + 1
+}
+##params <- houses %>%
+##  html_element(".zsg-photo-card-info") %>%
+##  html_text() %>%
+##  strsplit("\u00b7")
+prices
+# 
