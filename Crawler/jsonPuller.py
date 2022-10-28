f37e381c5b30c0e379882b44a34998f14be09c48
matfitchell <64942086+matfitchell@users.noreply.github.com>
2022-10-27 17:45:36 -0700

    Create jsonPuller.py

---
+++ Crawler/jsonPuller.py
@@ -0,0 +1,16 @@
+import http.client
+
+conn = http.client.HTTPSConnection("www.zillow.com")
+
+headersList = {
+ "Accept": "*/*",
+ "User-Agent": "Thunder Client (https://www.thunderclient.com)" 
+}
+
+payload = ""
+
+conn.request("GET", "/search/GetSearchPageState.htm?searchQueryState=%7B%22pagination%22%3A%7B%22currentPage%22%3A2%7D%2C%22usersSearchTerm%22%3A%22Los%20Angeles%2C%20CA%22%2C%22mapBounds%22%3A%7B%22west%22%3A-119.44444734375%2C%22east%22%3A-113.01744539062501%2C%22south%22%3A32.757531675134366%2C%22north%22%3A35.104452906128245%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A12447%2C%22regionType%22%3A6%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22beds%22%3A%7B%22min%22%3A1%7D%2C%22baths%22%3A%7B%22min%22%3A1%7D%2C%22sortSelection%22%3A%7B%22value%22%3A%22globalrelevanceex%22%7D%2C%22isAllHomes%22%3A%7B%22value%22%3Atrue%7D%2C%22isManufactured%22%3A%7B%22value%22%3Afalse%7D%2C%22isLotLand%22%3A%7B%22value%22%3Afalse%7D%2C%22isTownhouse%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A8%7D&wants={%22cat1%22:[%22listResults%22,%22mapResults%22],%22cat2%22:[%22total%22]}&requestId=19", payload, headersList)
+response = conn.getresponse()
+result = response.read()
+
+print(result.decode("utf-8"))
