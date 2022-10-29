efe370eae62f6786b61ff18641c8fd5d206f05e6
matfitchell <64942086+matfitchell@users.noreply.github.com>
2022-10-29 12:22:08 -0700

    Update jsonPuller.py

--- Crawler/jsonPuller.py
+++ Crawler/jsonPuller.py
@@ -1,10 +1,20 @@
 import http.client
-from urllib.parse import urlencode
+try:
+    import urlparse
+    from urllib import urlencode
+except: # For Python 3
+    import urllib.parse as urlparse
+    from urllib.parse import urlencode
 
+pageNumber = 1
 
-queryState = '"{"pagination":{"currentPage":2},"usersSearchTerm":"Los Angeles,CA","mapBounds":{"west":-119.44444734375,"east":-113.01744539062501,"south":32.757531675134366,"north":35.104452906128245},"regionSelection":[{"regionId":12447,"regionType":6}],"isMapVisible":True,"filterState":{"beds":{"min":1},"baths":{"min":1},"sortSelection":{"value":"globalrelevanceex"},"isAllHomes":{"value":True},"isManufactured":{"value":False},"isLotLand":{"value":False},"isTownhouse":{"value":False}},"isListVisible":True,"mapZoom":8}"'
+boilerPlate = '/search/GetSearchPageState.htm?searchQueryState='
 
-wants = '"{"pagination":{"currentPage":2},"usersSearchTerm":"Los Angeles,CA","mapBounds":{"west":-119.44444734375,"east":-113.01744539062501,"south":32.757531675134366,"north":35.104452906128245},"regionSelection":[{"regionId":12447,"regionType":6}],"isMapVisible":True,"filterState":{"beds":{"min":1},"baths":{"min":1},"sortSelection":{"value":"globalrelevanceex"},"isAllHomes":{"value":True},"isManufactured":{"value":False},"isLotLand":{"value":False},"isTownhouse":{"value":False}},"isListVisible":True,"mapZoom":8}"'
+queryState = '{"pagination":{"currentPage":2},"usersSearchTerm":"Los Angeles, CA","mapBounds":{"west":-119.44444734375,"east":-113.01744539062501,"south":32.757531675134366,"north":35.104452906128245},"regionSelection":[{"regionId":12447,"regionType":6}],"isMapVisible":True,"filterState":{"beds":{"min":1},"baths":{"min":1},"sortSelection":{"value":"globalrelevanceex"},"isAllHomes":{"value":True},"isManufactured":{"value":False},"isLotLand":{"value":False},"isTownhouse":{"value":False}},"isListVisible":True,"mapZoom":8}'
+
+wants = '{"pagination":{"currentPage":2},"usersSearchTerm":"Los Angeles, CA","mapBounds":{"west":-119.44444734375,"east":-113.01744539062501,"south":32.757531675134366,"north":35.104452906128245},"regionSelection":[{"regionId":12447,"regionType":6}],"isMapVisible":True,"filterState":{"beds":{"min":1},"baths":{"min":1},"sortSelection":{"value":"globalrelevanceex"},"isAllHomes":{"value":True},"isManufactured":{"value":False},"isLotLand":{"value":False},"isTownhouse":{"value":False}},"isListVisible":True,"mapZoom":8}'
+
+requestId = '&requestId=19'
 
 conn = http.client.HTTPSConnection("www.zillow.com")
 
@@ -15,7 +25,7 @@ headersList = {
 
 payload = ""
 
-conn.request("GET", "/search/GetSearchPageState.htm?searchQueryState=" + queryState + "&wants=" + wants + "&requestId=19", payload, headersList)
+conn.request("GET", boilerPlate+queryState+wants+requestId, payload, headersList)
 response = conn.getresponse()
 result = response.read()
 
