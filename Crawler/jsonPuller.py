349fbc549f094620f67bcef250a8045be2146452
matfitchell <64942086+matfitchell@users.noreply.github.com>
2022-10-30 23:05:41 -0700

    iojn

--- Crawler/jsonPuller.py
+++ Crawler/jsonPuller.py
@@ -5,11 +5,40 @@ import time
 
 for pageNumber in range(1,21):
     print('printing page number : %s' %pageNumber)
-    filename = './new_JSON_read_files/json_file_pg_%s.json' %pageNumber
-    #LA 
-    queryState = {"pagination":{"currentPage":pageNumber},"usersSearchTerm":"Los Angeles, CA","mapBounds":{"west":-119.44444734375,"east":-113.01744539062501,"south":32.757531675134366,"north":35.104452906128245},"regionSelection":[{"regionId":12447,"regionType":6}],"isMapVisible":True,"filterState":{"beds":{"min":1},"baths":{"min":1},"sortSelection":{"value":"globalrelevanceex"},"isAllHomes":{"value":True},"isManufactured":{"value":False},"isLotLand":{"value":False},"isTownhouse":{"value":False}},"isListVisible":True,"mapZoom":8}
 
-    wants = {"cat1":["listResults","mapResults"],"cat2":["total"]}
+    #filename = './new_JSON_read_files/los_angeles_json_file_pg_%s.json' %pageNumber
+
+    #filename = './new_JSON_read_files/dallas_json_file_pg_%s.json' %pageNumber
+
+    #filename = './new_JSON_read_files/new_york_json_file_pg_%s.json' %pageNumber
+
+    #filename = './new_JSON_read_files/chicago_json_file_pg_%s.json' %pageNumber
+
+    filename = './new_JSON_read_files/seattle_json_file_pg_%s.json' %pageNumber
+    
+    
+    #LA 
+    #queryState = {"pagination":{"currentPage":pageNumber},"usersSearchTerm":"Los Angeles, CA","mapBounds":{"west":-119.44444734375,"east":-113.01744539062501,"south":32.757531675134366,"north":35.104452906128245},"regionSelection":[{"regionId":12447,"regionType":6}],"isMapVisible":True,"filterState":{"beds":{"min":1},"baths":{"min":1},"sortSelection":{"value":"globalrelevanceex"},"isAllHomes":{"value":True},"isManufactured":{"value":False},"isLotLand":{"value":False},"isTownhouse":{"value":False}},"isListVisible":True,"mapZoom":8}
+
+   #wants = {"cat1":["listResults","mapResults"],"cat2":["total"]}
+
+   #Dallas
+    #queryState = {"pagination":{"currentPage":pageNumber},"usersSearchTerm":"Dallas, TX","mapBounds":{"west":-97.04728318457032,"east":-96.5075798154297,"south":32.70764142860342,"north":32.92807631002532},"regionSelection":[{"regionId":38128,"regionType":6}],"isMapVisible":True,"filterState":{"sortSelection":{"value":"globalrelevanceex"},"isAllHomes":{"value":True}},"isListVisible":True,"mapZoom":11}
+
+    #wants = {"cat1":["listResults","mapResults"],"cat2":["total"]}
+    #New York
+    #queryState = {"pagination":{"currentPage":pageNumber},"usersSearchTerm":"New York, NY","mapBounds":{"west":-75.05908773828125,"east":-72.90027426171875,"south":40.29893717780191,"north":41.0943914206361},"regionSelection":[{"regionId":6181,"regionType":6}],"isMapVisible":True,"filterState":{"sortSelection":{"value":"globalrelevanceex"},"isAllHomes":{"value":True}},"isListVisible":True,"mapZoom":9}
+
+   # wants = {"cat1":["listResults","mapResults"],"cat2":["total"]}
+   #Chicago
+    #queryState = {"pagination":{"currentPage":2},"usersSearchTerm":"Chicago, IL","mapBounds":{"west":-88.27164536914063,"east":-87.19223863085938,"south":41.638280450652054,"north":42.029147261767314},"regionSelection":[{"regionId":17426,"regionType":6}],"isMapVisible":True,"filterState":{"sortSelection":{"value":"globalrelevanceex"},"isAllHomes":{"value":True}},"isListVisible":True}
+
+    #wants ={"cat1":["listResults","mapResults"],"cat2":["total"]}
+
+    #Seattle
+    queryState ={"pagination":{},"usersSearchTerm":"Seattle, WA","mapBounds":{"west":-122.88449936914063,"east":-121.80509263085938,"south":47.43604571215944,"north":47.789693900848064},"regionSelection":[{"regionId":16037,"regionType":6}],"isMapVisible":True,"filterState":{"sortSelection":{"value":"globalrelevanceex"},"isAllHomes":{"value":True}},"isListVisible":True}
+
+    wants = {"cat1":["mapResults"]}
 
     params = {'searchQueryState' : queryState, 'wants' : wants}
 
